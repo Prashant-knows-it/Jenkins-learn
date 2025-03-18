418941be16094e48df7119b99465ebe053f943f7
Prashant Kumar <Huffingtoniscool@gmail.com>
2025-03-18 09:27:03 +0530

    removed second logger

--- src/test/java/com/example/Jenkins_learn/JenkinsLearnApplicationTests.java
+++ src/test/java/com/example/Jenkins_learn/JenkinsLearnApplicationTests.java
@@ -15,7 +15,6 @@ class JenkinsLearnApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing ");
-		logger.info("Test case executing second time");
 		assertEquals(true, true);
 	}
 
